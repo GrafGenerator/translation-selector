9d3ee787a4795a8dabdd8b51902d4b5bb54fc26c
nikita ivanov <ni.ivanov@2gis.ru>
2016-02-26 18:52:19 +0600

    playing with vs2015/typescript/aspnet5/etc

---
+++ src/src/Translation.UI/wwwroot/ts/greeter.js
@@ -0,0 +1,5 @@
+function greeter(name) {
+    return "Hi, " + name;
+}
+var user = "GrafGenerator";
+document.body.innerHTML = greeter(user);
---
+++ src/src/Translation.UI/wwwroot/ts/greeter.ts
@@ -0,0 +1,7 @@
+﻿function greeter(name: string): string {
+    return "Hi, " + name;
+}
+
+var user = "GrafGenerator";
+
+document.body.innerHTML = greeter(user);
